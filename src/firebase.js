70d90b0b17f954ff210802895e2f6b503cec8814
Mini-Anon <tusharbhardwaj2617@gmail.com>
2025-10-12 23:34:56 +0530

    Move Firebase credentials to .env file for security

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,14 @@
-// src/firebase.js
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// This is the object you just copied from the Firebase console
 const firebaseConfig = {
-  apiKey: "AIzaSyAhER8MvPEhaCjEUFYqWmORbYRszavZ5t4",
-  authDomain: "simple-canvas-editor-fb2f4.firebaseapp.com",
-  projectId: "simple-canvas-editor-fb2f4",
-  storageBucket: "simple-canvas-editor-fb2f4.firebasestorage.app",
-  messagingSenderId: "815852108733",
-  appId: "1:815852108733:web:52b1ab1f449038a68ef0d4"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-// This 'db' export is what you will use throughout your app to interact with Firestore
 export const db = getFirestore(app);
